94c90649e10cae67326434fa53c00900fbc72e66
vitalik poliakov <vitalik211287@gmail.com>
2022-02-07 21:35:07 +0200

    modal

--- js/modal.js
+++ js/modal.js
@@ -1,14 +1,23 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
-    modal: document.querySelector('[data-modal]'),
+    openHeaderModalBtn: document.querySelector("[data-header-modal-open]"),
+    closeHeaderModalBtn: document.querySelector("[data-header-modal-close]"),
+    headerModal: document.querySelector("[data-header-modal]"),
+    openModalBtn: document.querySelector("[data-modal-open]"),
+    closeModalBtn: document.querySelector("[data-modal-close]"),
+    modal: document.querySelector("[data-modal]"),
   };
+  refs.openHeaderModalBtn.addEventListener("click", toggleHeaderModal);
+  refs.closeHeaderModalBtn.addEventListener("click", toggleHeaderModal);
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
 
   function toggleModal() {
-    refs.modal.classList.toggle('is-hidden');
+    refs.modal.classList.toggle("is-hidden");
+  }
+
+  function toggleHeaderModal() {
+    refs.headerModal.classList.toggle("is-hidden");
   }
 })();
